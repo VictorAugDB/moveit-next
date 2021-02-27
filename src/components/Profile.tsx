13c927f0f66bfe8975500bf128cc41fdf9c5a60d
Victor Augusto <vdbernardes@gmail.com>
2021-02-27 12:36:30 -0300

    Aplicação básica finalizada

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://avatars.githubusercontent.com/u/50158294?s=460&u=902e52b40079476d2b49d0aa507d1a7a387f64a5&v=4" alt="Victor Augusto"/>
+      <div>
+        <strong>Victor Augusto</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level {level}
+        </p>
+      </div>
+    </div>
+  )
+}
