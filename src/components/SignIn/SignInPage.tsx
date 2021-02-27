2f663aa9c662fc3d668554c75e0ed2c4b6a94f89
Victor Augusto <vdbernardes@gmail.com>
2021-02-27 17:59:33 -0300

    página de login finalizada

--- src/components/SignIn/SignInPage.tsx
+++ src/components/SignIn/SignInPage.tsx
@@ -11,10 +11,11 @@ export function SignInPage() {
         <div>
           <h1>Bem-vindo</h1>
           <div className={styles.githubLogin}>
+            <img src="/icons/github.svg" alt="github_icon"/>
             <p>Faça login com seu github para começar</p>
           </div>
           <div>
-            <input type="text"/>
+            <input type="text" placeholder="Digite seu username"/>
             <button></button>
           </div>
         </div>
